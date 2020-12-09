d43a1397e18972aeb46e5252bb34023aef8cd447
rlilla <ricardo_lilla@hotmail.com>
2020-12-08 21:16:40 -0300

    main

--- src/main.cpp
+++ src/main.cpp
@@ -19,9 +19,14 @@ const char* pwd="caio1711";
 // Configuração do medidor de vazão 
 int pulsoLitro = 5880;
 // Variável para contagem dos pulsos
-int contador;
+int contadorEntrada = 0;
+int contadorSaida = 0;
+
 // Variável para contagem em litros
 float litros=0;
+float litrosEntrada = 0;
+float litrosSaida = 0;
+
 
 String values="{\"Volume Atual\":321}";
 parametros par;
@@ -34,9 +39,12 @@ telas t1;
 int numTela;
 
 
-void IRAM_ATTR trataInterrupacao(){
-  contador++;
-} 
+void IRAM_ATTR trataIntEntrada(){
+  contadorEntrada++;
+}
+void IRAM_ATTR trataIntSaida(){
+  contadorSaida++;
+}
 
 void taskServer(void *pvParameter);
 void taskScreens(void *pvParameter);
@@ -55,8 +63,9 @@ void setup() {
   }
   t1.ip=WiFi.localIP().toString();
   Serial.println(WiFi.localIP());
-  attachInterrupt(35,trataInterrupacao,RISING);  
-  
+  attachInterrupt(SENSOR_ENTRADA_INPUT,trataIntEntrada,RISING);  
+  attachInterrupt(SENSOR_SAIDA_INPUT,trataIntSaida,RISING);  
+
   xTaskCreatePinnedToCore(taskScreens,"Screen Task",configMINIMAL_STACK_SIZE + 1024,NULL,2,NULL,0);
   xTaskCreatePinnedToCore(taskServer,"Server Task",configMINIMAL_STACK_SIZE + 1024,NULL,4,NULL,0);
   xTaskCreatePinnedToCore(taskOperation,"Operation Task",configMINIMAL_STACK_SIZE + 1024,NULL,10,NULL,1);
@@ -68,7 +77,8 @@ void taskScreens(void *pvParameter){
   int tela=0;
   while(true){
     vTaskDelay(pdMS_TO_TICKS(500));
-    t1.volumeEntradaAtual = litros;
+    t1.volumeEntradaAtual = litrosEntrada;
+    t1.volumeSaidaAtual = litrosSaida;
     t1.volumeEntradaProgramado = 2.12;
     if(op.stModo.modoAuto) tela=0;
     if(op.stModo.modoManual) tela=2;
@@ -85,26 +95,28 @@ void taskServer(void *pvParameter){
 
 void taskOperation(void *pvParameter){
   while(true){
-    vTaskDelay(pdMS_TO_TICKS(1000));
-    op.loopOperacao();
-    op.stDadosOperacao.volumeEntradaAtual = litros;
+    op.stDadosOperacao.volumeEntradaAtual = litrosEntrada;
+    op.stDadosOperacao.volumeSaidaAtual = litrosSaida;
     op.stDadosOperacao.volumeEntradaProg = 3;
     op.stOperacao.iniciaSemiEntrada = true;
+    op.loopOperacao();
     atualizaSaidas();
+    vTaskDelay(pdMS_TO_TICKS(1000));
   }
 }
 
 void taskCalculations(void *pvParameter){
   while(true){
     vTaskDelay(pdMS_TO_TICKS(50));  
-    litros = (float) contador / (float) pulsoLitro;
-    valoresAtuais.volumeEntrada = litros;
+    litrosEntrada = (float) contadorEntrada / (float) pulsoLitro;
+    litrosSaida = (float) contadorSaida / (float) pulsoLitro;
+    valoresAtuais.volumeEntrada = litrosEntrada;
     valoresAtuais.diferencaMaxima = valoresAtuais.volumeEntrada - valoresAtuais.volumeSaidaInicial;
   }
 }
 
 void loop() {
-  if(Serial.read() > 0) contador=0;
+  if(Serial.read() > 0) contadorEntrada=0;
   delay(1000);
   Serial.println(numTela);
 }
